1e9efee4f958cf7c000bffc226830663de16c8e3
Riya12 <88799268+Riyatikole@users.noreply.github.com>
2023-10-25 20:28:47 +0530

    push

--- server/routes/messagesRoute.js
+++
@@ -1,9 +0,0 @@
-const {createMessage, getAllMessage } = require("../controllers/messagesController");
-
-
-const router = require("express").Router();
-
-router.post("/createmsg/", createMessage);
-router.post("/getmsg/", getAllMessage);
-
-module.exports = router;
